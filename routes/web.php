c8734c832fc2004e5a265fe366341b5b635f4121
vdonskweb <vdonskweb@yandex.ru>
2023-06-22 15:35:08 +0300

    lesson 4

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 
 Route::get('about', function () {
-    return view('about');
+	
+	$team = [
+		['name' => 'Hodor', 'position' => 'programmer'],
+		['name' => 'Joker', 'position' => 'CEO'],
+		['name' => 'Elvis', 'position' => 'CTO'],
+	];
+	
+    return view('about', ['team' => $team]);
 });
 
