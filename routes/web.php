b7667e7c6440cb3980541cd46a77a4628ed43e93
vdonskweb <vdonskweb@yandex.ru>
2023-06-23 13:07:19 +0300

    Создание сущности в CRUD

--- routes/web.php
+++ routes/web.php
@@ -46,5 +46,12 @@ Route::get('articles', [ArticleController::class, 'index'])
   ->name('articles.index');
 
 
+Route::get('articles/create', [ArticleController::class, 'create'])
+  ->name('articles.create');
+// POST-запрос
+Route::post('articles', [ArticleController::class, 'store'])
+  ->name('articles.store');
+Route::get('articles/{id}', [ArticleController::class, 'show'])
+  ->name('articles.show');
 
 
